01413f6779d3f76697125052bed9cd6717017bef
LeL-nardo <sandroleonardoads@gmail.com>
2022-11-11 15:45:25 -0300

    teste
    testando tamanhos de tela

---
+++ extra_screen-size/index.js
@@ -0,0 +1,8 @@
+function qualTamanho()  {
+    const screen = document.getElementById("screensize")
+    let width = window.innerWidth
+    let height = window.innerHeight
+    screen.innerHTML = `${width} x ${height}`
+}
+
+qualTamanho()
